5b5ca129ecda94903f56a0792fb4c13303733cb0
aksek <kosekaniela@gmail.com>
2020-11-22 10:58:48 +0100

    refactored variables names

--- src/BFS_solver.cpp
+++ src/BFS_solver.cpp
@@ -16,16 +16,16 @@ BFS_solver::BFS_solver(vector<vector<int> > init) {
     int c = 1;
     for (int i = 0; i < puzzleSize; i++) {
         for (int j = 0; j < puzzleSize; j++) {
-            goal.position[i][j] = c;
+            goal.grid[i][j] = c;
             c++;
         }
     }
-    goal.position[puzzleSize - 1][puzzleSize - 1] = -1;
-    goal.y_empty = puzzleSize - 1;
-    goal.x_empty = puzzleSize - 1;
+    goal.grid[puzzleSize - 1][puzzleSize - 1] = -1;
+    goal.y_blank = puzzleSize - 1;
+    goal.x_blank = puzzleSize - 1;
 }
 
-vector<BFS_solver::Node> BFS_solver::BFS(){
+vector<BFS_solver::Node> BFS_solver::solve(){
     queue<Node*> toCheck;
     toCheck.push(root);
     while(!toCheck.empty()) {
@@ -35,84 +35,95 @@ vector<BFS_solver::Node> BFS_solver::BFS(){
             vector<Node> path;
             path.push_back(*current);
             while(current->parent != nullptr) {
-                cout << "(" << current->y_empty << ", " << current->x_empty << ") ";
-                cout << ((current->lastMove == Direction::Up) ? "UP" : ((current->lastMove == Direction::Right) ? "RIGHT" : ((current->lastMove == Direction::Down) ? "DOWN" : "LEFT"))) << endl;
+                cout << "(" << current->y_blank << ", " << current->x_blank << ") ";
+                cout << ((current->last_move == Direction::Up) ? "UP" : 
+                ((current->last_move == Direction::Right) ? "RIGHT" : 
+                ((current->last_move == Direction::Down) ? "DOWN" : "LEFT"))) << endl;
                 current = current->parent;
                 path.push_back(*current);
             }
             return path;
         }
-        if (current->y_empty < puzzleSize - 1) {
-            Node *nextUp = moveUp(current);
-            if (!graph.count(*nextUp)) {
-                graph.insert(*nextUp);
-                toCheck.push(nextUp);
+        if (current->y_blank < puzzleSize - 1) {
+            Node *next_up = moveUp(current);
+            if (!graph.count(*next_up)) {
+                graph.insert(*next_up);
+                toCheck.push(next_up);
             }
         }
-        if (current->y_empty > 0) {
-            Node *nextDown = moveDown(current);
-            if (!graph.count(*nextDown)) {
-                graph.insert(*nextDown);
-                toCheck.push(nextDown);
+        if (current->y_blank > 0) {
+            Node *next_down = moveDown(current);
+            if (!graph.count(*next_down)) {
+                graph.insert(*next_down);
+                toCheck.push(next_down);
             }
         }
-        if (current->x_empty > 0) {
-            Node *nextRight = moveRight(current);
-            if (!graph.count(*nextRight)) {
-                graph.insert(*nextRight);
-                toCheck.push(nextRight);
+        if (current->x_blank > 0) {
+            Node *next_right = moveRight(current);
+            if (!graph.count(*next_right)) {
+                graph.insert(*next_right);
+                toCheck.push(next_right);
             }
         }
-        if (current->x_empty < puzzleSize - 1) {
-            Node *nextLeft = moveLeft(current);
-            if (!graph.count(*nextLeft)) {
-                graph.insert(*nextLeft);
-                toCheck.push(nextLeft);
+        if (current->x_blank < puzzleSize - 1) {
+            Node *next_left = moveLeft(current);
+            if (!graph.count(*next_left)) {
+                graph.insert(*next_left);
+                toCheck.push(next_left);
             }
         }
     }
 }
 BFS_solver::Node* BFS_solver::moveUp(Node *current) {
-    return new Node(current, current->x_empty, current->y_empty + 1, Direction::Up);
+    return new Node(current, current->x_blank, current->y_blank + 1, Direction::Up);
 }
 BFS_solver::Node* BFS_solver::moveRight(Node *current) {
-    return new Node(current, current->x_empty - 1, current->y_empty, Direction::Right);
+    return new Node(current, current->x_blank - 1, current->y_blank, Direction::Right);
 }
 BFS_solver::Node* BFS_solver::moveDown(Node *current) {
-    return new Node(current, current->x_empty, current->y_empty - 1, Direction::Down);
+    return new Node(current, current->x_blank, current->y_blank - 1, Direction::Down);
 }
 BFS_solver::Node* BFS_solver::moveLeft(Node *current) {
-    return new Node(current, current->x_empty + 1, current->y_empty, Direction::Left);
+    return new Node(current, current->x_blank + 1, current->y_blank, Direction::Left);
 }
 
 BFS_solver::Node::Node(Node *prev, int x, int y, Direction lm) {
     parent = prev;
-    y_empty = y;
-    x_empty = x;
-    position = prev->position;
-    int tile = position[y_empty][x_empty];
-    position[prev->y_empty][prev->x_empty] = tile;
-    position[y_empty][x_empty] = -1;
-    lastMove = lm;
+    y_blank = y;
+    x_blank = x;
+    grid = prev->grid;
+    int tile = grid[y_blank][x_blank];
+    grid[prev->y_blank][prev->x_blank] = tile;
+    grid[y_blank][x_blank] = -1;
+    last_move = lm;
 }
 BFS_solver::Node::Node(vector<vector<int> > pos) {
     parent = nullptr;
-    position = pos;
-    lastMove = Direction::None;
+    grid = pos;
+    last_move = Direction::None;
     for (int i = 0; i < pos.size(); i++) {
         for (int j = 0; j < pos.size(); j++) {
-            if (position[i][j] == -1) {
-                y_empty = i;
-                x_empty = j;
+            if (grid[i][j] == -1) {
+                y_blank = i;
+                x_blank = j;
             }
         }
     }
 }
 bool BFS_solver::Node::operator==(const Node &that) const {
-    for (int i = 0; i < this->position.size() && i < that.position.size(); i++) {
-        for (int j = 0; j < this->position[i].size() && j < that.position[i].size(); j++) {
-            if (this->position[i][j] != that.position[i][j]) return false;
+    for (int i = 0; i < this->grid.size() && i < that.grid.size(); i++) {
+        for (int j = 0; j < this->grid[i].size() && j < that.grid[i].size(); j++) {
+            if (this->grid[i][j] != that.grid[i][j]) return false;
         }
     }
     return true;
+}
+
+std::size_t BFS_solver::node_hash::operator()(const Node &N) const {
+    boost::hash<std::vector<int> > vector_hash;
+    std::vector<int> row_hashes;
+    for (int i = 0; i < N.grid.size(); i++) {
+        row_hashes.push_back(vector_hash(N.grid[i]));
+    }
+    return vector_hash(row_hashes);
 }
--- src/BFS_solver.hpp
+++ src/BFS_solver.hpp
@@ -3,7 +3,7 @@
 
 #include<vector>
 #include<unordered_set>
-#include <boost/functional/hash.hpp>
+#include<boost/functional/hash.hpp>
 
 
 class BFS_solver {
@@ -11,35 +11,28 @@ class BFS_solver {
 
     struct Node {
         Node *parent;
-        int y_empty, x_empty;
-        std::vector<std::vector<int> > position;
-        Direction lastMove;
+        int y_blank, x_blank;
+        std::vector<std::vector<int> > grid;
+        Direction last_move;
 
         Node() {}
         Node(std::vector<std::vector<int> > pos);
         Node(Node *prev, int x, int y, Direction lm);
         bool operator==(const Node &that) const;
     };
-    struct myHash {
-        std::size_t operator()(const Node &N) const {
-            boost::hash<std::vector<int> > vectorHash;
-            std::vector<int> rowHashes;
-            for (int i = 0; i < N.position.size(); i++) {
-                rowHashes.push_back(vectorHash(N.position[i]));
-            }
-            return vectorHash(rowHashes);
-        }
+    struct node_hash {
+        std::size_t operator()(const Node &N) const;
     };
 
     Node *root;
-    std::unordered_set<Node, myHash> graph;
+    std::unordered_set<Node, node_hash> graph;
     Node goal;
     int puzzleSize;
 
 public:
     BFS_solver(std::vector<std::vector<int> > init);
 
-    std::vector<Node> BFS();
+    std::vector<Node> solve();
 
     Node* moveUp(Node *current);
     Node* moveRight(Node *current);
