904637481bd6d2d8f44fc4514f85e54cae8633cf
baciar <baciar112@gmail.com>
2020-12-01 08:56:39 +0100

    Fixed-up memory leaks in A*

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<vector>
+#include <vector>
 #include <set>
 
 #include"BFS_solver.hpp"
@@ -7,31 +7,44 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
-    std::cout << "Hello, World!!!" << std::endl;
 
+    if(argc == 4)
+    {
+        //*generowanie itd
+        int mode = atoi(argv[1]);
+        bool displayPath = atoi(argv[2]) == 1;
+
+    }
+    else if(argc == 1)
+    {
+
+    }
+    */
+   /* std::cout << "Hello, World!!!" << std::endl;
+    clock_t start = clock();
     vector<vector<int> > testV = {
-        {-1, 1, 3},
-        { 4, 2, 5},
-        { 7, 8, 6}
+            {2, 5, 3, 4},
+            {9, 1, 7, 8},
+            {-1, 6, 10, 12},
+            {13, 14, 11, 15}
     };
     BFS_solver solver1 = BFS_solver(testV);
     solver1.solve();
-  
+    std::cout << "czas wykonania " << ( clock() - start ) / CLOCKS_PER_SEC<< std::endl;
+
     std::vector<std::vector<int>> board =
             {
-                    {5, 9, 3, 4},
-                    {6, 10, 14, 8},
-                    {0, 13, 2, 15},
-                    {11, 7, 12, 1}
+                    {2, 5, 3, 4},
+                    {9, 1, 7, 8},
+                    {-1, 6, 10, 12},
+                    {13, 14, 11, 15}
             };
     auto solver2 = new A_star_solver(board);
-    clock_t start = clock();
+    auto start = clock();
     std::cout << solver2->solve() << std::endl;
     std::cout << "czas wykonania " << ( clock() - start ) / CLOCKS_PER_SEC<< std::endl;
     solver2->printSolution();
-    //std::cout << 5861044965 / CLOCKS_PER_SEC;
-
     return 0;
 }
