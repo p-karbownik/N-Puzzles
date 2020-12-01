26b7d43529fa58040aea084a7df91388a2a065ed
aksek <kosekaniela@gmail.com>
2020-12-01 19:24:36 +0100

    fixed all memory leaks in BFS

--- src/BFS_solver.hpp
+++ src/BFS_solver.hpp
@@ -2,6 +2,7 @@
 #define NPUZZLES_BFS_SOLVER
 
 #include<vector>
+#include<queue>
 #include<unordered_set>
 #include<boost/functional/hash.hpp>
 
@@ -11,11 +12,13 @@ class BFS_solver {
     struct Node {
         Node *parent;
         int y_blank, x_blank;
+        Node* children[4];
         std::vector<std::vector<int> > grid;
 
         Node() {}
         Node(std::vector<std::vector<int> > pos);
         Node(Node *prev, int x, int y);
+        ~Node();
         bool operator==(const Node &that) const;
     };
     struct node_hash {
@@ -24,10 +27,13 @@ class BFS_solver {
 
     Node *root;
     std::unordered_set<Node, node_hash> graph;
+    std::queue<Node*> toCheck;
     Node goal;
     int puzzleSize;
     const static int blank_value = -1;
     std::vector<Node*> solution;
+
+    void clear(std::queue<Node*> &Q);
 
     Node* move_up(Node *current);
     Node* move_right(Node *current);
@@ -35,6 +41,7 @@ class BFS_solver {
     Node* move_left(Node *current);
 public:
     BFS_solver(std::vector<std::vector<int> > init);
+    ~BFS_solver();
 
     bool solve();
     std::vector<std::vector<std::vector<int> > > get_solution();
